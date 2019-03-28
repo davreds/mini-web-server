a8ea1b78a9339e6573344e6d6362d4477a9c6b4b
DavReds <dav_rojas@hotmail.com>
2019-03-28 16:55:16 -0600

    Some changes

--- omdb.js
+++ omdb.js
@@ -1,55 +1,60 @@
-const credentials = require('./credentials.js')
 const request = require('request')
 
+if ( process.env.NODE_ENV === 'prodction') {
+    var APIKEY = prcess.env.APIKEY
+} else {
+    const credentials = require('./credentials.js')
+    var APIKEY = credentials.apikey
+}
+
 const omdbMovie = function( title, callback ) {
-  const url = 'http://www.omdbapi.com/?t=' + title + 
-  '&apikey=' + credentials.apikey
-  console.log(url)
-  request({ url, json: true }, function(error, response ) {
-    if (error) {
-      callback('Service unavailable', undefined)
-    } else if ( response.body.Response == 'False' ) {
-      callback(response.body.Error, undefined)
-    } else {
-      const data = response.body
-      const info = {
-        title: data.Title,
-        plot: data.Plot,
-        rating: data.Ratings[0].Value,
-        seasons: data.totalSeasons
-      }
-      callback(undefined, info)
-    }
-  })
+    const url = 'http://www.omdbapi.com/?t=' + title +
+    '&apikey=' + APIKEY.apikey
+    console.log(url)
+    request({ url, json: true }, function(error, response ) {
+        if (error) {
+            callback('Service unavailable', undefined)
+        } else if ( response.body.Response == 'False' ) {
+            callback(response.body.Error, undefined)
+        } else {
+            const data = response.body
+            const info = {
+                title: data.Title,
+                plot: data.Plot,
+                rating: data.Ratings[0].Value,
+                seasons: data.totalSeasons
+            }
+            callback(undefined, info)
+        }
+    })
 }
 
 const omdbSeason = function( title, seasonNo, callback ) {
-  const url = 'http://www.omdbapi.com/?t=' + title + 
-  '&Season=' + seasonNo +
-  '&apikey=' + credentials.apikey 
-  request({ url, json: true }, function(error, response ) {
-    if (error) {
-      callback('Service unavailable', undefined)
-    } else if ( response.body.Response == 'False' ) {
-      callback(response.body.Error, undefined)
-    } else {
-      const data = response.body
-      const info = {
-        title: data.Title,
-        season: data.Season,
-        episodes : []
-      }
-      for( i in data.Episodes ) {
-        info.episodes.push(data.Episodes[i].Title)
-      }
-      callback(undefined, info)
-    }
-  })
+    const url = 'http://www.omdbapi.com/?t=' + title +
+    '&Season=' + seasonNo +
+    '&apikey=' + APIKEY.apikey
+    request({ url, json: true }, function(error, response ) {
+        if (error) {
+            callback('Service unavailable', undefined)
+        } else if ( response.body.Response == 'False' ) {
+            callback(response.body.Error, undefined)
+        } else {
+            const data = response.body
+            const info = {
+                title: data.Title,
+                season: data.Season,
+                episodes : []
+            }
+            for( i in data.Episodes ) {
+                info.episodes.push(data.Episodes[i].Title)
+            }
+            callback(undefined, info)
+        }
+    })
 }
 
 
 module.exports = {
-  omdbMovie: omdbMovie,
-  omdbSeason: omdbSeason
+    omdbMovie: omdbMovie,
+    omdbSeason: omdbSeason
 }
-
